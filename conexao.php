e00e9d4a76cf054cbef4736f0cfaf5c40ed02091
hallyson007 <hallysonmartins@gmail.com>
2024-01-28 20:31:34 -0300

    modificado e atual

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$hostname = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'cafeteria';
+$port = '3306';
+
+$conexao = mysqli_connect($hostname, $username, $password, $database, $port);
+
+
